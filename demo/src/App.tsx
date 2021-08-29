0f77f1a661a7a13a9a76ea50e71ac4fa4bd98736
Pierre Bertet <hello@pierre.world>
2021-08-29 21:57:09 +0100

    Tweak demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -7,35 +7,37 @@ function App() {
   const itemsWithRarities = useItemRarity(bag?.items ?? []);
 
   return (
-    <div>
-      <h1>Loot Rarity Check</h1>
-      <h2>
-        <label htmlFor="bag-input">Bag #</label>
-        <input
-          id="bag-input"
-          value={bagId ?? ""}
-          onChange={(event) => {
-            const value = event.currentTarget.value.trim();
-            if (value === "") {
-              setBagId("");
-              return;
-            }
+    <div className="app">
+      <div className="header">
+        <h1>Loot Rarity Check</h1>
+        <h2>
+          <label htmlFor="bag-input">Bag #</label>
 
-            const numId = Number(value);
-            if (!isNaN(numId) && numId > 0 && numId <= 8000) {
-              setBagId(value);
-            }
-          }}
-        />
-        <button onClick={() => setBagId(randomBagId())}>random</button>
-      </h2>
+          <input
+            id="bag-input"
+            value={bagId ?? ""}
+            onChange={(event) => {
+              const value = event.currentTarget.value.trim();
+              if (value === "") {
+                setBagId("");
+                return;
+              }
+              const numId = Number(value);
+              if (!isNaN(numId) && numId > 0 && numId <= 8000) {
+                setBagId(value);
+              }
+            }}
+          />
+          <button onClick={() => setBagId(randomBagId())}>random</button>
+        </h2>
+      </div>
       {itemsWithRarities.length > 0 && (
-        <ul>
+        <ul className="bag">
           {itemsWithRarities.map(
             ({ color = "#ffffff", name, description = "…" }, index) => {
               return (
                 <li key={name + index} style={{ color }}>
-                  {name} ({description})
+                  {name} ({description.slice(0, 1)})
                 </li>
               );
             }
